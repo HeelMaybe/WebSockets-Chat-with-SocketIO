1f18aa5627166094cd43b7753583ca0313246c12
HeelMaybe <ghelton1@uncc.edu>
2022-03-31 13:35:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    "C:/Users/Graham/Documents/UNCC/Spring-2022/ITIS-6177/week 11/index.html"
+  );
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
